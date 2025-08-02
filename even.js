fd40af59a2ea496be8cf3d25206a95b2dad34297
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-08-03 02:39:17 +0600

    added even.js file

--- even.js
+++ even.js
@@ -3,3 +3,23 @@
 // Find all the even numbers from 78 to 98.
 
 //  */
+
+let num = 78;
+// // while loop
+while (num <= 98) {
+  console.log(num);
+  if (num % 2 === 0) {
+    console.log('even number', num);
+  }
+  num++;
+}
+
+// for loop
+
+for (let num = 78; num <= 98;){
+  console.log(num);
+  if (num % 2 === 0) {
+    console.log('even number', num);
+  }
+  num++;
+}
