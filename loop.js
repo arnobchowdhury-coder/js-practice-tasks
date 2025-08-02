ea7c0c5a75b83c8e98bd740a375eced4534ba996
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-08-03 02:06:19 +0600

    added for loop

--- loop.js
+++ loop.js
@@ -7,10 +7,17 @@
 /*programming hero*/
 
 let num = 0;
+// while loop
 while (num <= 60) {
   console.log('I will invest at least 6 hrs every single day for next 60 days!', num);
   num++;
   
 }
+// for loop
+for (let num = 0; num <= 60; num++){
+  console.log('I will invest at least 6 hrs every single day for next 60 days!', num);
+}
+
+
 
 
