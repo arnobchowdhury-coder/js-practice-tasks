39fdf3bd06404855023f5da754341621f0a216f7
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-08-03 02:31:44 +0600

    some changes odd.js file

---
+++ even.js
@@ -0,0 +1,5 @@
+// Subtask-2:
+
+// Find all the even numbers from 78 to 98.
+
+//  */
--- odd.js
+++ odd.js
@@ -7,15 +7,7 @@ Find all the odd numbers from 61 to 100.
  */
 
 
-/***
 
-Subtask-2:
-
-Find all the even numbers from 78 to 98.
-
- */
-
-/*programming hero*/
 
 
 let number = 61;
